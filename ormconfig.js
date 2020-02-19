fd3128ed7bc110b1622055c078578982be6443ba
boatnoodle <nattasit31@gmail.com>
2020-02-19 07:16:22 +0700

    🔧 Update config database

--- ormconfig.js
+++ ormconfig.js
@@ -21,7 +21,11 @@ if (NODE_ENV !== "production") {
 } else {
   module.exports = {
     type: "postgres",
-    url: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
     synchronize: false,
     entities: ["src/models/*.ts"],
     migrations: ["src/migration/**/*.ts"],
